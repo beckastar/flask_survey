221045eed44a775981b6198d5dff04dddf0948fb
Becka <rebecca.robbins.et@gmail.com>
2014-12-22 17:37:29 -0800

    stripping down to resolve 404

--- app/forms.py
+++ app/forms.py
@@ -3,15 +3,7 @@ from wtforms import Form, fields,  validators, ValidationError, TextField, Integ
 # from wtforms import widgets, StringField, BooleanField, RadioField
 # from wtforms.validators import DataRequired, Email, Length, Regexp
 
-class UserForm(Form):
-    cyclist_name    = TextField('Username', [validators.Length(min=4, max=25)])
-    cyclist_email  = TextField('Email Address', [validators.Length(min=6, max=35)])
-    cyclist_age = IntegerField('User Level', [validators.NumberRange(min=0, max=80)])
-
-
-class IncidentReportForm(Form):
-    name = TextField("Name")
-    email = TextField ("Email address")
-    injury = fields.RadioField('How badly were you hurt?', choices=[('0','Minor scrapes or bruises'), ('1', 'More severely'), ('2', 'Hospitalized or incapacitated'), ('3', 'This was a fatal crash')], validators = [DataRequired()], default=None)
-    age = fields.RadioField('What is your age?', choices=[('lt18', 'Younger than 18'), ('18-24', '18 to 24'), ('25-34', '25 to 34'),
-    ('35-44', '35 to 44'), ('45-54', '45 to 54'), ('55', '55 years or older')], validators=[DataRequired()])
+class PersonForm(Form):
+    name    = TextField('Username', [validators.Length(min=4, max=25)])
+    email  = TextField('Email Address', [validators.Length(min=6, max=35)])
+    age = IntegerField('User Level', [validators.NumberRange(min=0, max=80)])
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 
 from app import db
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, scoped_session
 
@@ -12,115 +12,15 @@ session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
-ROLE_USER = 0
-ROLE_ADMIN = 1
-
 Base = declarative_base()
 Base.query = session.query_property()
 
 class User(db.Model):
     __tablename__="user"
     id = db.Column(db.Integer, primary_key = True)
-    cyclist_name = db.Column(db.String(15))
-    cyclist_email = db.Column(db.String(25))
-    cyclist_age= db.Column(db.Integer)
-    date_created = db.Column(db.String(64), nullable=False)
-
-    # Return true unless user not allowed to authenticate
-    def is_authenticated(self):
-        return True
-
-    # Returns False for a banned user
-    def is_active(self):
-        return True
-
-    # Return true for fake users not supposed to login
-    def is_anonymous(self):
-        return False
-
-    # Generate unique id for user
-    def get_id(self):
-        return unicode(self.id)
-
-    #Salt password
-    @staticmethod
-    def set_password(password):
-        return generate_password_hash(password)
-
-    def check_password(self, submitted_pwd):
-        return check_password_hash(self.password, submitted_pwd)
-
-
-class Crash_Incident(db.Model):
-    __tablename__='crash_incident'
-    id = db.Column(db.Integer, primary_key = True)
-    injury_severity = db.Column(db.String(50))
-    type_of_bike = db.Column(db.String(50))
-    name_of_street = db.Column(db.String(50))
-    building_address = db.Column(db.Integer)
-    cross_street =db.Column(db.String(50))
-    year_of_crash = db.Column(db.Integer, default = 2015)
-    month_of_crash =db.Column(db.Integer)
-    day_of_week = db.Column(db.String(10))
-    approx_time = db.Column(db.Integer)
-    rider = db.Column(db.Boolean, default=True)
-    holiday = db.Column(db.Boolean, default = False)
-    road_conditions = db.Column(db.Boolean, default= False)
-    vehicle_violations = db.Column(db.Boolean, default = False)
-    lighting_conditions = db.Column(db.Boolean, default= False)
-    road_surface = db.Column(db.Boolean, default=False)
-    another_vehicle = db.Column(db.Boolean, default=True)
-    other = db.Column(db.String(50))
-
-    def __repr__(self):
-        return self.cyclist_name
-
-class Road_Quality(db.Model):
-    __tablename__='road_quality'
-    id = db.Column(Integer, primary_key=True)
-    incident_id = db.Column(Integer, ForeignKey('crash_incident.id'))
-    muni_tracks = db.Column(db.Boolean)
-    potholes = db.Column(db.Boolean)
-    loose_materials_on_roadway = db.Column(db.Boolean)
-    obstruction_on_roadway = db.Column(db.Boolean)
-    construction = db.Column(db.Boolean)
-    reduced_roadway_width = db.Column(db.Boolean)
-    flooded = db.Column(db.Boolean)
-    other_roadway_issue  = db.Column(db.String(50))
-
-    def __repr__(self):
-        return self.incident_id
-
-class Other_Vehicle(db.Model):
-    __tablename__='other_vehicle'
-    id = db.Column(Integer, primary_key=True)
-    incident_id = db.Column(Integer, ForeignKey('crash_incident.id'))
-    bike =db.Column(db.Boolean, default = False)
-    car = db.Column(db.Boolean, default = False)
-    motorcycle = db.Column(db.Boolean, default = False)
-    bus = db.Column(db.Boolean, default = False)
-    muni = db.Column(db.Boolean, default = False)
-
-
-class Vehicle_Violation(db.Model):
-    __tablename__='vehicle_violation'
-    id = db.Column(Integer, primary_key=True)
-    incident_id = db.Column(Integer, ForeignKey('crash_incident.id'))
-    stopped = db.Column(db.Boolean)
-    driving_straight = db.Column(db.Boolean)
-    ran_off_road = db.Column(db.Boolean)
-    turning_right = db.Column(db.Boolean)
-    turning_left = db.Column(db.Boolean)
-    u_turn = db.Column(db.Boolean)
-    backing_up = db.Column(db.Boolean)
-    changing_lanes = db.Column(db.Boolean)
-    slowing_down = db.Column(db.Boolean)
-    entering_traffic = db.Column(db.Boolean)
-    parking = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return self.incident_id
-
+    name = db.Column(db.String(15))
+    email = db.Column(db.String(25))
+    age= db.Column(db.Integer)
 
 def main():
     Base.metadata.create_all(ENGINE)
